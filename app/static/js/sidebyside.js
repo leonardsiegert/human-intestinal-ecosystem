4f5d43294fcd7bcf0c38c26335e0333c0954e75e
Leonard Siegert <leonard.siegert96@gmail.com>
2025-07-08 17:27:33 +0200

    Added a mini-header for sidebyside with responsive layout and styling

--- app/static/js/sidebyside.js
+++ app/static/js/sidebyside.js
@@ -1,14 +1,13 @@
 d3.select("#resetSelection")
-        .style("visibility", "visible")
-        .on("click", function(event){
-            rightvis.window.document.getElementById("resetSelection").click()
-            leftvis.window.document.getElementById("updateData").click()
-            middlevis.window.document.getElementById("updateSelection").click()
-			})
+    .style("visibility", "visible")
+    .on("click", function (event) {
+        rightvis.window.document.getElementById("resetSelection").click()
+        leftvis.window.document.getElementById("updateData").click()
+        middlevis.window.document.getElementById("updateSelection").click()
+    })
 
 d3.select("#updateData")
-        .on("click", function(event){
-            middlevis.window.document.getElementById("updateSelection").click()
-            leftvis.window.document.getElementById("updateData").click()
-            });
-
+    .on("click", function (event) {
+        middlevis.window.document.getElementById("updateSelection").click()
+        leftvis.window.document.getElementById("updateData").click()
+    });
